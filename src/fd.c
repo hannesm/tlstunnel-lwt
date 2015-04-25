ee35c5e133c987937cb4554c9b6efe59c0946c7a
Hannes Mehnert <hannes@mehnert.org>
2015-04-25 17:01:04 +0100

    use open instead of socket to really get the POSIX defined lowest :)

--- src/fd.c
+++ src/fd.c
@@ -1,12 +1,12 @@
 #include <caml/mlvalues.h>
 
 #include <unistd.h>
-#include <sys/socket.h>
+#include <fcntl.h>
 
 CAMLprim value
 caml_next_fd (value unit) {
   int fd ;
-  fd = socket(PF_INET, SOCK_STREAM, 0) ;
+  fd = open("/", O_RDONLY) ;
   close(fd) ;
   return Val_int(fd);
 }
